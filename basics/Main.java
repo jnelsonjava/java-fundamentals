149df53b3ec615a25bff2c330ea546659ef239f4
Jack Nelson <jnelson.java@gmail.com>
2020-09-14 15:48:19 -0700

    added coin flip method

--- basics/Main.java
+++ basics/Main.java
@@ -1,8 +1,13 @@
+import java.util.*;
+
 public class Main {
   public static void main(String[] args) {
     // test run of pluralize method
     int dogCount = 2;
     System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+    // test run of flipNHeads method
+    flipNHeads(2);
 
     
   }
@@ -13,5 +18,23 @@ public class Main {
     } else {
       return word;
     }
+  }
+
+  public static void flipNHeads(int n) {
+    int totalFlips = 0;
+    int consecutiveHeads = 0;
+
+    // reference for random numbers https://www.geeksforgeeks.org/generating-random-numbers-in-java/
+    while (consecutiveHeads < n) {
+      totalFlips++;
+      if (Math.random() >= 0.5) {
+        consecutiveHeads++;
+        System.out.println("heads");
+      } else {
+        consecutiveHeads = 0;
+        System.out.println("tails");
+      }
+    }
+    System.out.println("It took " + totalFlips + " flips to flip " + n + " " + pluralize("head", n) + " in a row.");
   }
 }
