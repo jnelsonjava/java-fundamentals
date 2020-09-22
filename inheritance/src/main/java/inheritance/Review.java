c7c32861b3b10c9e89b8c6d12e694ee70a4d6486
Jack Nelson <jnelson.java@gmail.com>
2020-09-22 11:03:46 -0700

    refactored classes

--- inheritance/src/main/java/inheritance/Review.java
+++ inheritance/src/main/java/inheritance/Review.java
@@ -2,22 +2,39 @@ package inheritance;
 
 public class Review {
 
-    String author;
-    String body;
-    int stars;
+    private String author;
+    private String body;
+    private int stars;
+    private Restaurant restaurant;
 
-    public Review(String author, String body, int stars) {
+    public Review(String author, String body, int stars, Restaurant restaurant) {
         this.author = author;
         this.body = body;
         this.stars = stars;
+        this.restaurant = restaurant;
+
+        restaurant.addReview(this);
     }
 
     @Override
     public String toString() {
         return "Review{" +
-                "author='" + author + '\'' +
+                "restaurant='" + restaurant.getName() + '\'' +
+                ", author='" + author + '\'' +
                 ", stars=" + stars +
                 ", body='" + body + '\'' +
                 '}';
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public int getStars() {
+        return stars;
     }
 }
--- inheritance/src/test/java/inheritance/ReviewTest.java
+++ inheritance/src/test/java/inheritance/ReviewTest.java
@@ -6,14 +6,17 @@ import static org.junit.Assert.assertEquals;
 
 public class ReviewTest {
     @Test public void testNewReview() {
-        Review testReview = new Review("Bob", "It's good!", 4);
-        assertEquals("the instance's author field should be set", "Bob", testReview.author);
-        testReview = new Review("Amy", "It's no good!", 1);
-        assertEquals("the instance's body field should be set", "It's no good!", testReview.body);
+        Restaurant testRestaurant = new Restaurant("Zippy's", 1);
+        Review testReview = new Review("Bob", "It's good!", 4, testRestaurant);
+        assertEquals("the instance's author field should be set", "Bob", testReview.getAuthor());
+        assertEquals("the restaurant list should include this review", testReview, testRestaurant.getReviewList().getFirst());
+        testReview = new Review("Amy", "It's no good!", 1, testRestaurant);
+        assertEquals("the instance's body field should be set", "It's no good!", testReview.getBody());
     }
 
     @Test public void testToString() {
-        Review testReview = new Review("Bob", "It's good!", 4);
-        assertEquals("instance should output pretty print", "Review{author='Bob', stars=4, body='It's good!'}", testReview.toString());
+        Restaurant testRestaurant = new Restaurant("Zippy's", 1);
+        Review testReview = new Review("Bob", "It's good!", 4, testRestaurant);
+        assertEquals("instance should output pretty print", "Review{restaurant='Zippy's', author='Bob', stars=4, body='It's good!'}", testReview.toString());
     }
 }
